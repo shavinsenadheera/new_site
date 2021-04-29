7572040fd6d7fb0ed5238c580fb2f07665042109
Shavindu Senadheera <shavinsenadeera@gmail.com>
2021-04-29 12:20:26 +0530

    first commit

---
+++ resources/lang/en/welcome.php
@@ -0,0 +1,42 @@
+<?php
+    return [
+        'home'                      =>'Home',
+        'features'                  =>'Features',
+        'our_brains'                =>'Our brains',
+        'about_us'                  =>'About us',
+        'contact_us'                =>'Contact us',
+        'sec_features_notice_1'     =>'Our Features and Services',
+        'sec_features_notice_2'     =>'We are using the latest technologies and methods to build your software solutions.',
+        'sec_features_feature_1'    =>'Modern Design',
+        'sec_features_feature_2'    =>'Clean Design',
+        'sec_features_feature_3'    =>'More Dynamic',
+        'sec_features_feature_4'    =>'Free SEO',
+        'sec_features_feature_1_dsc'=>'We are using modern web design to make your web solution.',
+        'sec_features_feature_2_dsc'=>'Designing your website with clean and modern design.',
+        'sec_features_feature_3_dsc'=>'You can manage and handle your own webpage without 3rd party engagement.',
+        'sec_features_feature_4_dsc'=>'We will give free SEO for your web applications to get best achievement for your business.',
+        'technologies' => [
+            'topic'             => 'Our Brains',
+            'description'       => 'We are using the latest technologies and methods to build your software solutions.',
+        ],
+        'aboutus'  => [
+            'topic'             => 'Learn More About Us',
+            'introduction'      =>'We are professionals in customized creative website development in Sri Lanka.',
+            'mission'=> [
+                'topic'         =>'Mission',
+                'description'   =>'We are professionals in customized creative website development in Sri Lanka.',
+            ],
+            'vision'=> [
+                'topic'         =>'Vision',
+                'description'   =>'To be a sustainable and successful venture in the web development field of Sri Lanka.',
+            ],
+        ],
+        'contact'=> [
+            'topic'             => 'Get In Touch',
+            'description'       => 'We are also in Social Media Network. You can visit to us using following links.',
+        ],
+        'reserved'=>[
+            'notice'            => 'All Right Reserved RabbitDevs',
+        ]
+    ];
+?>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8" />
+    <title>RabbitDevs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/icon.png') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-5.0.0-alpha-2.min.css')}}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/LineIcons.2.0.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+</head>
+<body>
+<div class="preloader">
+    <div class="loader">
+        <div class="ytp-spinner">
+            <div class="ytp-spinner-container">
+                <div class="ytp-spinner-rotator">
+                    <div class="ytp-spinner-left">
+                        <div class="ytp-spinner-circle"></div>
+                    </div>
+                    <div class="ytp-spinner-right">
+                        <div class="ytp-spinner-circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<header class="header">
+    <div class="navbar-area">
+        <div class="container px-5">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand text-white text-uppercase" href="index.html">
+                            <img src="{{ asset('img/icon.png') }}" alt="" width="50px">
+                        </a>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                            <ul id="nav" class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="page-scroll" href="#home">{{ __('welcome.home') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="page-scroll" href="#features">{{ __('welcome.features') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="page-scroll" href="#brains">{{ __('welcome.our_brains') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="page-scroll" href="#about">{{ __('welcome.about_us') }}</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="page-scroll" href="#contact">{{ __('welcome.contact_us') }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<section id="home" class="hero-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="hero-img wow fadeInUp" data-wow-delay=".5s">
+                    <img src="{{ asset('img/welcome1.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="features" class="service-section pt-10">
+    <div class="container">
+        <div class="row align-items-center bg-light p-3">
+            <div class="col-xl-6 col-lg-9">
+                <div class="service-content">
+                    <div class="section-title">
+                        <h1 class="mb-30 wow fadeInUp" data-wow-delay=".2s">
+                            {{ __('welcome.sec_features_notice_1') }}
+                        </h1>
+                        <p class="mb-40 wow fadeInUp" data-wow-delay=".4s">
+                            {{ __('welcome.sec_features_notice_2') }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 col-xl-6">
+                <div class="row">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="single-service">
+                            <div class="icon">
+                                    <svg fill="#286ade"  version="1.1" id="Layer_2_1_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve">
+                                        <path d="M47.7,6.7L47.7,6.7C42.9,2.1,36.8-0.2,30.4,0C18.5,0.8,8.9,11.2,8.9,23.6c0,8,3.5,15.4,9.6,19.9v11.2c0,5,4,9.3,9,9.3h8.8
+                                            c4.8,0,9-4,9-9.3V43.6c6.1-4.5,9.8-11.7,9.8-19.6C54.8,17.5,52.4,11.2,47.7,6.7z M36,58.4h-8.8c-2.1,0-3.7-1.6-3.7-4v-9.3h15.9v9.3
+                                            C39.7,56.8,38.1,58.4,36,58.4z M41,39.8L41,39.8h-6.6v-7.7l5.8-4.5c1.1-0.8,1.3-2.7,0.5-3.7c-0.8-1.1-2.7-1.3-3.7-0.5l-2.7,2.1v-4.2
+                                            c0-1.6-1.1-2.7-2.7-2.7c-1.6,0-2.7,1.1-2.7,2.7v4.5l-3.7-2.4c-1.3-0.8-2.9-0.5-3.7,0.8c-0.8,1.3-0.5,2.9,0.8,3.7l6.6,4.5v7.4h-6.6
+                                            c-5.3-3.5-8.5-9.6-8.5-16.2c0-9.8,7.4-17.8,16.5-18.3c0.3,0,0.8,0,1.1,0c4.5,0,9,1.9,12.2,5c3.5,3.7,5.6,8.5,5.6,13.5
+                                            C49.5,30.5,46.3,36.4,41,39.8z"/>
+                                    </svg>
+                            </div>
+                            <div class="content">
+                                <h3>{{ __('welcome.sec_features_feature_1') }}</h3>
+                                <p>{{ __('welcome.sec_features_feature_1_dsc') }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-6 col-md-6">
+                        <div class="single-service">
+                            <div class="icon">
+                                <svg fill="none" height="40" viewBox="0 0 64 58" width="64" xmlns="http://www.w3.org/2000/svg"><path d="m62.1 3.00001c-2.7-2.700002-7-2.700003-9.6-.3l-34.8 32.39999c-1.1 0-2.4.3-3.7.8-2.4 1.1-3.5 2.9-4.5 4.5-1.1 1.9-2.1 3.7-5.6 5.9l-3.9 2.5 3.7 2.1c2.1 1.3 10.2 6.2 17.1 6.2 2.1 0 4-.5 5.6-1.6 2.4-1.9 3.7-4.3 3.5-7v-.3l32.1-35.3c2.8-2.99999 2.5-7.19999.1-9.89999zm-38.8 48.19999c-2.1 1.6-8.3-.3-13.4-2.7 1.9-1.9 2.9-3.7 3.7-5.1.8-1.3 1.3-2.1 1.9-2.4 1.9-.8 3.7 0 4 0 2.4 1.9 5.1 4.8 5.1 7.5.3.5 0 1.6-1.3 2.7zm34.8-42.09999-30 33.19999c-1.3-2.1-3.2-4-4.8-5.4l32.6-30.19999c.5-.5 1.6-.5 2.1 0 .9.6.9 1.6.1 2.4z" fill="#286ade"/></svg>
+                            </div>
+                            <div class="content">
+                                <h3>{{ __('welcome.sec_features_feature_2') }}</h3>
+                                <p>{{ __('welcome.sec_features_feature_2_dsc') }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-6 col-md-6">
+                        <div class="single-service">
+                            <div class="icon">
+                                <svg fill="#286ade" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                     viewBox="0 0 64 64" style="enable-background:new 0 0 64 64;" xml:space="preserve">
+                                    <path d="M55.7,52l-7.2-15.7c2.4-2.4,4.3-5.1,5.6-8.3c0.5-1.3-0.3-2.9-1.6-3.5c-1.3-0.5-2.9,0.3-3.5,1.6c-0.8,1.9-1.9,3.5-2.9,5.1
+                                        l-4.3-9.9c0.8-1.3,1.1-2.9,1.1-4.8c0-5.1-3.5-9.3-8.3-10.4V2.7C34.7,1.1,33.6,0,32,0c-1.6,0-2.7,1.1-2.7,2.7v3.7
+                                        c-4.8,1.1-8.3,5.3-8.3,10.4c0,1.6,0.5,3.2,1.1,4.8l-4.3,9.3c-1.3-1.6-2.4-3.2-2.9-5.1c-0.5-1.3-2.1-2.1-3.5-1.6
+                                        c-1.3,0.5-2.1,2.1-1.6,3.5c1.3,3.2,3.2,5.9,5.6,8.3L8.3,51.7L8,60.8c0,1.1,0.5,2.4,1.6,2.9c0.5,0,0.8,0.3,1.3,0.3
+                                        c0.8,0,1.6-0.3,2.1-0.8l6.7-6.1l6.7-14.9c1.9,0.5,3.7,0.8,5.6,0.8c1.9,0,3.7-0.3,5.6-0.8l6.9,14.9l6.4,6.1c0.5,0.5,1.3,0.8,2.1,0.8
+                                        c0.5,0,1.1,0,1.3-0.3c1.1-0.5,1.6-1.6,1.6-2.9L55.7,52z M15.2,53.9l-1.6,1.6v-2.1l6.1-13.9c0.5,0.3,1.1,0.5,1.6,0.8L15.2,53.9z
+                                         M23.5,35.5c-0.5-0.3-1.1-0.5-1.6-0.8l4-8.8c0.5,0.3,1.1,0.5,1.6,0.8L23.5,35.5z M32,37.6c-1.1,0-2.1-0.3-3.5-0.3l3.5-7.5l3.5,7.5
+                                        C34.1,37.3,33.1,37.6,32,37.6z M32,22.1c-3.2,0-5.6-2.4-5.6-5.6s2.4-5.6,5.6-5.6s5.6,2.4,5.6,5.6S35.2,22.1,32,22.1z M40.5,35.5
+                                        l-4-8.8c0.5-0.3,1.1-0.5,1.6-0.8l4,8.8C41.3,34.9,41.1,35.2,40.5,35.5z M48.8,53.9l-6.1-13.6c0.5-0.3,1.1-0.5,1.6-0.8l6.4,13.9v2.1
+                                        L48.8,53.9z"/>
+                                </svg>
+                            </div>
+                            <div class="content">
+                                <h3>{{ __('welcome.sec_features_feature_3') }}</h3>
+                                <p>{{ __('welcome.sec_features_feature_3_dsc') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="single-service">
+                            <div class="icon">
+                                <i class="lni lni-seo"></i>
+                            </div>
+                            <div class="content">
+                                <h3>{{ __('welcome.sec_features_feature_4') }}</h3>
+                                <p>{{ __('welcome.sec_features_feature_4_dsc') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="brains" class="service-section pt-20">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12 col-xl-6">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-laravel lni-80 text-danger"></i>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-php lni-80 text-info"></i>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-java lni-80 text-black-50"></i>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-react lni-80 text-primary"></i>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-bootstrap lni-80 text-primary"></i>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 text-center">
+                        <div class="p-5">
+                            <i class="lni lni-wordpress-filled lni-80 text-dark"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-9">
+                <div class="service-content">
+                    <div class="section-title">
+                        <h1 class="mb-30 wow fadeInUp" data-wow-delay=".2s">
+                            {{ __('welcome.technologies.topic') }}
+                        </h1>
+                        <p class="mb-40 wow fadeInUp" data-wow-delay=".4s">
+                            {{ __('welcome.technologies.description') }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="about" class="about-section pt-20">
+    <div class="container">
+        <div class="row align-items-center bg-light p-3">
+            <div class="col-lg-6">
+                <div class="about-content">
+                    <div class="section-title">
+                        <h1 class="wow fadeInUp" data-wow-delay=".2s">{{ __('welcome.aboutus.topic') }}</h1>
+                        <div class="wow fadeInUp" data-wow-delay=".4s">
+                            <h6>{{ __('welcome.aboutus.introduction') }}</h6><br>
+                            <h5>{{ __('welcome.aboutus.mission.topic') }}</h5>
+                            <p>{{ __('welcome.aboutus.mission.description') }}</p>
+                            <h5>{{ __('welcome.aboutus.vision.topic') }}</h5>
+                            <p>{{ __('welcome.aboutus.vision.description') }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-img wow fadeInUp" data-wow-delay=".5s">
+                    <img class="img-fluid" src="{{ asset('assets/img/about/about-img.svg') }}" alt="About us">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="contact" class="contact-section pt-20">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-xl-5">
+                <div class="section-title text-center mb-60">
+                    <h1 class="mb-25 wow fadeInUp" data-wow-delay=".2s">{{ __('welcome.contact.topic') }}</h1>
+                    <p class="wow fadeInUp" data-wow-delay=".4s">{{ __('welcome.contact.description') }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 justify-content-center text-center">
+                <div class="contact-img wow fadeInUp" data-wow-delay=".5s">
+                    <img class="img-fluid" src="{{asset('assets/img/contact/contact-img.svg')}}" alt="Contact us">
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                <div class="contact-wrapper wow fadeInUp item" data-wow-delay=".2s" id="contact">
+                    <ul class="align-self-auto text-center justify-content-center">
+                        <li>
+                            <a href="https://www.facebook.com/RabbitDevs" target="_blank">
+                                <i class="lni lni-facebook-filled" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/company/rabbitdevs/" target="_blank">
+                                <i class="lni lni-linkedin" aria-hidden="true"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer mt-50 pt-40">
+    <div class="container">
+        <div class="widget-wrapper">
+            <div class="row">
+                <div class="col-lg-12 col-sm-6">
+                    <div class="footer-widget">
+                        <ul class="socials text-center justify-content-center align-items-center">
+                            <li>
+                                <a href="https://www.facebook.com/RabbitDevs" target="_blank">
+                                    <i class="lni lni-facebook-filled"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/company/rabbitdevs/" target="_blank">
+                                    <i class="lni lni-linkedin-original"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="text-center pb-30" style="color: #fff">{{ __('welcome.reserved.notice') }} {{ now()->year }}</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<a href="#" class="scroll-top btn-hover">
+    <i class="lni lni-chevron-up"></i>
+</a>
+
+<script src="{{ asset('assets/js/bootstrap.5.0.0.alpha-2-min.js') }}"></script>
+<script src="{{ asset('assets/js/count-up.min.js') }}"></script>
+<script src="{{ asset('assets/js/wow.min.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
+</body>
+</html>
