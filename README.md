7e529042f957637763bd4b28bfd46026002451a2
Shavindu Senadheera <shavinsenadeera@gmail.com>
2021-04-29 16:48:12 +0530

    add messenger

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-<img src="https://rabbitdevs.com/img/icon.png" width="40">
-## RabbitDevs Software Solution 2021
+<img src="https://rabbitdevs.com/img/icon.png" width="40" alt="">
+<h1>RabbitDevs Software Solution 2021</h1>
 Copyright by rabbitdevs.com 2021
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,31 +11,6 @@
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
 </head>
 <body>
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v10.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-     attribution="page_inbox"
-     page_id="109543617641244">
-</div>
-
 <div class="preloader">
     <div class="loader">
         <div class="ytp-spinner">
@@ -345,5 +320,31 @@
 <script src="{{ asset('assets/js/count-up.min.js') }}"></script>
 <script src="{{ asset('assets/js/wow.min.js') }}"></script>
 <script src="{{ asset('assets/js/main.js') }}"></script>
+
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+<script type="javascript">
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution="page_inbox"
+     page_id="109543617641244">
+</div>
+
 </body>
 </html>
