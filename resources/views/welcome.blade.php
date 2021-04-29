64d31a75efd546b85a2a35b4cd2a2c49c4ac5894
Shavindu Senadheera <shavinsenadeera@gmail.com>
2021-04-29 16:24:59 +0530

    add messenger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,31 @@
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
 </head>
 <body>
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution="page_inbox"
+     page_id="109543617641244">
+</div>
+
 <div class="preloader">
     <div class="loader">
         <div class="ytp-spinner">
@@ -310,30 +335,6 @@
         </div>
     </div>
 </footer>
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v10.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-
-<!-- Your Chat Plugin code -->
-<div class="fb-customerchat"
-     attribution="page_inbox"
-     page_id="109543617641244">
-</div>
 
 <a href="#" class="scroll-top btn-hover">
     <i class="lni lni-chevron-up"></i>
